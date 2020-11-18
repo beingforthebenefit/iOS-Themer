aaec1caa32ceb7a7a4f59574b849dfc4c7b44818
Gerald Todd <gerald@gtodd.dev>
2020-11-18 02:23:33 -0800

    Add download module and screenshots for instructions.

--- application/views/home/download.php
+++ application/views/home/download.php
@@ -1,3 +1,18 @@
-<form action="?a=download" method="POST">
-    <input type="submit" name="submit" value="Download Theme" />
+<form class="form" method="POST" action="?a=download">
+    <fieldset class="form-fieldset">
+        <ol class="step-list">
+            <li class="step-item">
+                <div class="form-button-wrap">
+                    <input class="form-field-button" type="submit" name="submit" value="Download Theme" <?= empty($model) ? 'disabled' : '' ?>/>
+                </div>
+            </li>
+            <?php for ($i = 1; $i <= 3; $i++) { ?>
+                <li class="step-item">
+                    <div class="step-wrap">
+                        <img class="step-image" src="/images/steps/<?= $i ?>.png" title="Step <?= $i + 1 ?>" />
+                    </div>
+                </li>
+            <?php } ?>
+        </ol>
+    </fieldset>
 </form>
