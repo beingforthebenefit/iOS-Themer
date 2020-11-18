e5436e290fa60b02cfbfe7d7b95b5e81f4f1a5d2
Gerald Todd <gerald@gtodd.dev>
2020-11-18 02:24:54 -0800

    Add footer and 'buy me a beer' icon.

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -1,5 +1,16 @@
-<hr class="footer__sep" />
     <footer class="footer">
-        Copyright &copy; <?= date('Y') ?> Gerald Todd
+        <div class="footer-note">
+            <p>Made with love by <a href="https://github.com/beingforthebenefit" target="_BLANK">Gerald Todd</a></p>
+        </div>
+
+        <form class="donate-form" action="https://www.paypal.com/donate" method="post" target="_BLANK">
+            <input type="hidden" name="cmd" value="_donations" />
+            <input type="hidden" name="business" value="MLEYJQ92SGDJQ" />
+            <input type="hidden" name="currency_code" value="USD" />
+            <input type="hidden" name="amount" value="5" />
+            <label class="donate-label" for="donate" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button">Enjoy this site? Click here to buy me a beer</label>
+            <input id="donate" class="donate" type="image" src="/images/donate-icon.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+        </form>
     </footer>
 </body> 
