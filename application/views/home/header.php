6226fbed88e6e97ef497730ff53197a0ebe76838
Gerald Todd <gerald@gtodd.dev>
2020-11-20 01:38:52 -0800

    Add google analytics code, new js/css files, and viewport settings for mobile compatibility.

--- application/views/home/header.php
+++ application/views/home/header.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- For favicons -->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
@@ -11,14 +12,26 @@
     <link rel="manifest" href="/site.webmanifest">
     <!-- End favicons -->
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-KCM2YB8CJ6"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-KCM2YB8CJ6');
+    </script>
+
     <title>iDevice Icon Themer</title>
     <meta name="description" content="iDevice Icon Themer">
     <meta name="author" content="Gerald Todd">
 
     <script src="/js/file-upload.js"></script>
     <script src="/js/search.js"></script>
+    <script src="/js/tabs.js"></script>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/iphone.css">
+    <link rel="stylesheet" href="/css/iphone-switch.css">
 
 </head>
 
