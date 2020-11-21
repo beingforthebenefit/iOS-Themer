884c142b30a2212b41329013b6316b37dfb815a7
Gerald Todd <gerald@gtodd.dev>
2020-11-21 13:07:00 -0800

    Add button to remove icon through delete action.

--- application/views/home/icon.php
+++ application/views/home/icon.php
@@ -1,6 +1,11 @@
 <div class="app">
+    <form method="post">
+        <input type="hidden" name="a" value="delete" />
+        <input type="hidden" name="delete" value="<?= $icon->PayloadUUID ?>" />
+        <input class="app-close" type="submit" name="remove" value="&#8212;" />
+    </form>
     <img class="icon" src="data:image;base64,<?= $icon->Icon ?>" />
-    <p class="label" title="<?= $icon->Label ?>">
+    <p class="icon-label" title="<?= $icon->Label ?>">
         <?= $icon->Label ?>
     </p>
 </div>
