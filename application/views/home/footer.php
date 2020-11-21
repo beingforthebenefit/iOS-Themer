9e94c4d0549ae5af0f60616a94be314efee6e7d9
Gerald Todd <gerald@gtodd.dev>
2020-11-21 09:41:49 -0800

    Remove preset donation amount on paypal.

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -7,7 +7,6 @@
             <input type="hidden" name="cmd" value="_donations" />
             <input type="hidden" name="business" value="MLEYJQ92SGDJQ" />
             <input type="hidden" name="currency_code" value="USD" />
-            <input type="hidden" name="amount" value="5" />
             <label class="donate-label" for="donate" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button">Enjoy this site? Click here to buy me a beer</label>
             <input id="donate" class="donate" type="image" src="/images/donate-icon.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
             <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
