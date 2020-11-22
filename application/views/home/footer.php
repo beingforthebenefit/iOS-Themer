59444108a045550bb63d55bf461ae84bcf553a05
Gerald Todd <gerald@gtodd.dev>
2020-11-21 20:00:11 -0800

    Add twitter link to footer

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -1,7 +1,10 @@
     <footer class="footer">
         <div class="footer-note">
             <p>Made with love by <a href="https://github.com/beingforthebenefit" target="_BLANK">Gerald Todd</a></p>
+            <a href="https://twitter.com/intent/tweet?screen_name=iOSThemeLive&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">Tweet to @iOSThemeLive</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
+
+            
 
         <div class="donate-wrap" action="https://www.buymeacoffee.com/iostheme">
             <a class="donate-label" href="https://www.buymeacoffee.com/iostheme" title="Buy me a beer" alt="Buy me a beer" target="_BLANK">
