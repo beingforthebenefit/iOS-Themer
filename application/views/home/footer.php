0893eded02246f7bb19f7c8e97dde5d14142b6d5
Gerald Todd <gerald@gtodd.dev>
2020-11-21 19:34:23 -0800

    Update donation service.

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -3,13 +3,10 @@
             <p>Made with love by <a href="https://github.com/beingforthebenefit" target="_BLANK">Gerald Todd</a></p>
         </div>
 
-        <form class="donate-form" action="https://www.paypal.com/donate" method="post" target="_BLANK">
-            <input type="hidden" name="cmd" value="_donations" />
-            <input type="hidden" name="business" value="MLEYJQ92SGDJQ" />
-            <input type="hidden" name="currency_code" value="USD" />
-            <label class="donate-label" for="donate" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button">Enjoy this site? Click here to buy me a beer</label>
-            <input id="donate" class="donate" type="image" src="/images/donate-icon.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-        </form>
+        <div class="donate-wrap" action="https://www.buymeacoffee.com/iostheme">
+            <a class="donate-label" href="https://www.buymeacoffee.com/iostheme" title="Buy me a beer" alt="Buy me a beer" target="_BLANK">
+                Enjoy this site? Click here to buy me a beer  <img class="donate" src="/images/donate-icon.png" />
+            </a>
+        </div>
     </footer>
 </body> 
