c025d9af91c353567ebef104a935444f01d529d0
Gerald Todd <gerald@gtodd.dev>
2020-11-26 09:57:55 -0800

    Refine/correct model class for api keys with methods to add and verify keys.

--- application/models/ApiKeyModel.class.php
+++ application/models/ApiKeyModel.class.php
@@ -2,35 +2,25 @@
 // application/models/ApiKeyModel.class.php
 
 class ApiKeyModel extends Model {
-    
-    // repositories :: void -> [[string -> a]]|false
-    public function repositories() {
-        $sql = "SELECT * FROM {$this->table} ORDER BY `stars` DESC";
-        $repositories = $this->db->rows($sql);
-
-        return $repositories;
-    }
 
     // addKey :: (string, string, string) -> bool
     public function addKey($owner, $key, $expiration) {
         return $this->insert([
-            'key' => $key;
-            'owner' => $owner;
-            'dateCreated' => date('Y-m-d H:i:s');
-            'dateExpires' => $expiration;
+            'key' => $key,
+            'owner' => $owner,
+            'dateCreated' => date('Y-m-d H:i:s'),
+            'dateExpires' => $expiration,
         ]);
     }
 
-    // validateKey :: string -> bool
+    // validateKey :: filters -> bool
+    // filters = [string => a]
     public function validateKey($key) {
-        $sql = "SELECT * FROM {$this->table}";
-        $keys = $this->rows($sql);
-
         return !empty(
             array_filter(
-                $keys,
+                $this->rows(['key' => $key]),
                 function($validKey) use ($key) {
-                    return $validKey == $key;
+                    return $validKey['key'] == $key;
                 }
             )
         );
