a8d10ff2e4c6505fd1263f8e0cfd2ad5908ee0cc
Gerald Todd <gerald@gtodd.dev>
2020-11-28 13:43:59 -0800

    New install view for API batch installations.

---
+++ application/views/home/install.php
@@ -0,0 +1,11 @@
+    <div class="page-wrap">
+        <div class="phone-container" id="phone">
+            <h1 class="header">Preview</h1>
+            <?php include CURR_VIEW_PATH . 'phone.php' ?>
+        </div>
+
+        <div class="download-container">
+            <h1 class="header">Install On Your Device</h1>
+            <?php include CURR_VIEW_PATH . 'download.php' ?>
+        </div>
+    </div>
