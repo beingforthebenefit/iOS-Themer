4b1ea6e8f8a9d8ae11f510caa9e719093836dd25
Gerald Todd <gerald@gtodd.dev>
2020-11-30 10:06:13 -0800

    Add weather api

---
+++ public/weather/index.php
@@ -0,0 +1,16 @@
+<?php
+$city = $_GET['city'];
+$weather = shell_exec('curl "http://wttr.in/'. $city .'?u&format=%25C+%25f"');
+$fileName = md5(date('ymdhmsu')) . ".png";
+$cmd = "echo '{$weather}' | convert label:@- -trim {$fileName}";
+exec($cmd);
+$fp = fopen($fileName, 'rb');
+
+header('Content-Type: image/png');
+header("Content-Length: " . filesize($name));
+
+fpassthru($fp);
+
+fclose($fp);
+exec("rm {$fileName}");
+exit;
