1ec2455b954eafeca9fbd65c8d36dd69acd8c248
Gerald Todd <gerald@gtodd.dev>
2020-11-30 09:09:44 -0800

    Add time API

---
+++ public/time/index.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set($_GET['timezone']);
+$time = date('g:i a');
+$fileName = md5(date('ymdhmsu')) . ".png";
+$cmd = "echo '{$time}' | convert label:@- {$fileName}";
+exec($cmd);
+$fp = fopen($fileName, 'rb');
+
+header('Content-Type: image/png');
+header("Content-Length: " . filesize($name));
+
+fpassthru($fp);
+
+fclose($fp);
+exec("rm {$fileName}");
+exit;
