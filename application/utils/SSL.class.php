e926eb372749c0c1eab2153c69b16a677d92e784
Gerald Todd <gerald@gtodd.dev>
2020-11-29 23:59:28 -0800

    Fix dumb error.

--- application/utils/SSL.class.php
+++ application/utils/SSL.class.php
@@ -8,6 +8,7 @@ class SSL {
     // sign :: (string, string) -> void
     public static function sign($input, $output) {
         $sslPath = '/home/beingforthebenefit/certs';
-        shell_exec("openssl smime -sign -signer " . $sslPath . "/iostheme.crt -inkey " . $sslPath . "/iostheme.key -certfile " . $sslPath . "/chain.crt -nodetach -outform der -in " . $input . " out " . $output);
+        $cmd = "openssl smime -sign -signer " . $sslPath . "/iostheme.crt -inkey " . $sslPath . "/iostheme.key -certfile " . $sslPath . "/chain.crt -nodetach -outform der -in " . $input . " -out " . $output;
+        shell_exec($cmd);
     }
 }
