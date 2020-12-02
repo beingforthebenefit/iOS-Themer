27d140a3c84aa9432f7001ebbd889002af7e6c73
Gerald Todd <gerald@gtodd.dev>
2020-12-02 11:03:50 -0800

    Improve weather endpoint (hopefully) by using cURL

--- public/weather/index.php
+++ public/weather/index.php
@@ -1,6 +1,19 @@
 <?php
-$city = escapeshellarg($_GET['city']);
-$weather = shell_exec('curl "http://wttr.in/'. $city .'?u&format=%25C+%25f"');
+if (array_key_exists('city', $_GET)) {
+    $city = escapeshellarg($_GET['city']);
+} else {
+    header('Content-Type: text/plain');
+    echo "Please set a city";
+}
+
+$handle = curl_init();
+curl_setopt_array($handle, [
+    CURLOPT_URL => "http://wttr.in/{$city}?u&format=%25C+%25f",
+    CURLOPT_RETURNTRANSFER => true
+]);
+
+$weather = curl_exec($handle);
+
 $fileName = md5(date('ymdhmsu')) . ".png";
 $cmd = "echo '{$weather}' | convert label:@- -trim {$fileName}";
 exec($cmd);
@@ -11,6 +24,7 @@ header("Content-Length: " . filesize($fileName));
 
 fpassthru($fp);
 
+curl_close($handle);
 fclose($fp);
 exec("rm {$fileName}");
 exit;
