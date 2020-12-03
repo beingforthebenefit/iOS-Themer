c09e9179afa761b84bd152f6e802f26b9ca7fe75
doyourhomeworkjacob <75362648+doyourhomeworkjacob@users.noreply.github.com>
2020-12-04 01:56:57 +0530

    Corrected a Typo
    Changed iOS Themes to iOS Themer in the description

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -2,6 +2,6 @@
     iOS Icon Themer
 </h1>
 <div class="page-paragraph">
-    iOS Themes by 22over7 is a free web app that allows theming iOS icons in one fell swoop. Optimized for dekstop and mobile, our icons launch immediately with no pop-ups. Install instantly from your iDevice, or batch upload from your desktop.
+    iOS Themer by 22over7 is a free web app that allows theming iOS icons in one fell swoop. Optimized for dekstop and mobile, our icons launch immediately with no pop-ups. Install instantly from your iDevice, or batch upload from your desktop.
 </div>
 <button class="try-button" onclick="changePage('installer')">Start Theming!</button>
