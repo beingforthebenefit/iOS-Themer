9fd2e8c48309b30d458b6a9739a404ee88827bd2
Gerald Todd <gerald@gtodd.dev>
2020-12-06 14:58:41 -0800

    Add call to new method and change title of app rename stuff.

--- application/views/home/icon.php
+++ application/views/home/icon.php
@@ -5,7 +5,7 @@
         <input class="app-close" type="submit" name="remove" value="&#8212;" />
     </form>
     <img class="icon" src="data:image;base64,<?= $icon->Icon ?>" />
-    <p class="icon-label" title="<?= $icon->Label ?>">
+    <p class="icon-label" title="<?= $icon->Label ?>: Click to edit" onclick="editIcon('<?= $index ?>', '<?= $icon->Label ?>')">
         <?= $icon->Label ?>
     </p>
 </div>
