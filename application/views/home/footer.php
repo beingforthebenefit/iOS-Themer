125cc406e6ab35749a61bf0bec55014b26da86a6
doyourhomeworkjacob <75362648+doyourhomeworkjacob@users.noreply.github.com>
2020-12-07 08:05:00 +0530

    Added a ❤ sign and changed the footer text
    Made with love by Gerald Todd and Designed by Ubaid D ( was too lengthy ) I changed it to- Made with ❤ by Gerald Todd and Ubaid D
    Is that okay?

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -7,7 +7,7 @@
     <footer class="footer">
         <div class="footer-row">
             <div class="footer-note">
-                <p class="contact">Made with love by <a href="https://github.com/beingforthebenefit" target="_BLANK">Gerald Todd</a>. Designed by <a href="https://twitter.com/ubaidafedar" target="_BLANK">Ubaid D.</a></p>
+                <p class="contact">Made with ❤ by <a href="https://github.com/beingforthebenefit" target="_BLANK">Gerald Todd</a> and <a href="https://twitter.com/ubaidafedar" target="_BLANK">Ubaid D.</a></p>
             </div>
             <div class="footer-note">
                 <a href="https://twitter.com/intent/tweet?screen_name=iOSThemeLive&ref_src=twsrc%5Etfw" target="_BLANK">Tweet to @iOSThemeLive</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
