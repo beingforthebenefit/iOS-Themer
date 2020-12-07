6d392e60dd5af65f4addd3d462688dd9cf0f4c6a
doyourhomeworkjacob <75362648+doyourhomeworkjacob@users.noreply.github.com>
2020-12-07 07:55:18 +0530

    Changed some text in the How To page
    Also can you please tell me how do I move the How To and the text beneath it to the left?

--- application/views/home/how-to.php
+++ application/views/home/how-to.php
@@ -1,27 +1,27 @@
 <div class="page-paragraph">
     <h1 class="page-title">
-        How To
+        How To Use?
     </h1>
-    <h3>Simple. Intuitive. Fun.</h3>
+    <h3>Simple, Intuitive, Fun.</h3>
     <ol>
         <li>Choose the App Category</li>
         <li>Search for an App</li>
         <li>Select country and search for an app</li>
         <li>Select the correct app</li>
-        <li>Change the label to whatever you want</li>
-        <li>Drag and drop an icon into the box (or click in the box)</li>
+        <li>Change the label to whatever you want or leave it blank</li>
+        <li>Drag or click to import an icon</li>
         <li>Click "Add Icon"</li>
     </ol>
-    <h3>Add as many icons as you want and install.</h3>
+    <h3>Add as many icons as you want and click on 'Download Theme'.</h3>
 
 </div>
 <div class="video-wrap">
     <div class="video">
-        <h1 class="header">Batch Theming with a PC in Seconds</h1>
+        <h1 class="header">How to Batch theme using a PC in seconds!</h1>
         <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/ZKWvdFz1z38" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
     <div class="video">
-        <h1 class="header">Theming Directly From Your iPhone</h1>
+        <h1 class="header">How to use iOS Themer on an iPhone</h1>
         <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/BviPK5H4gNs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </div>
