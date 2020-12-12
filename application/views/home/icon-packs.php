06be00d67922e34fde83c23cf659ca625cc5263a
Gerald Todd <gerald@gtodd.dev>
2020-12-12 15:51:36 -0800

    Add dummy data to see new DOM and styles of icon pack page.

--- application/views/home/icon-packs.php
+++ application/views/home/icon-packs.php
@@ -1,1 +1,34 @@
-<h1 class="page-header">Coming Soon...</h1>
+<!-- TODO: Move this info to database and display entries automatically -->
+
+<div class="icon-pack-previews-wrap">
+    <?php for ($i = 0; $i < 10; $i++) { ?>
+    <div class="icon-pack-preview-wrap">
+
+        <!-- Icon pack image -->
+        <div class="icon-pack-preview-image-wrap">
+            <img class="icon-pack-preview-image" src="/images/icon-pack-previews/demo.jpg" />
+        </div>
+        <!-- end Icon pack image -->
+
+        <!-- Icon pack description -->
+        <div class="icon-pack-description-wrap">
+            <div class="icon-pack-title">
+                Demo Icon Pack
+            </div>
+            <div class="icon-pack-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
+            <div class="icon-pack-links">
+                <a class="icon-pack-link">
+                    Install now
+                </a><br />
+                <a class="icon-pack-link">
+                    Customize first
+                </a>
+            </div>
+        </div>
+        <!-- end Icon pack description -->
+
+    </div>
+    <?php } ?>
+</div>
