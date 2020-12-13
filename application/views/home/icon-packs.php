a10f7746bca1de73f8a040b8d36c83d0fff4cf52
Gerald Todd <gerald@gtodd.dev>
2020-12-12 19:14:44 -0800

    Remove dummy data, make links work.

--- application/views/home/icon-packs.php
+++ application/views/home/icon-packs.php
@@ -1,7 +1,6 @@
 <!-- TODO: Move this info to database and display entries automatically -->
 
 <div class="icon-pack-previews-wrap">
-    <?php for ($i = 0; $i < 10; $i++) { ?>
     <div class="icon-pack-preview-wrap">
 
         <!-- Icon pack image -->
@@ -13,16 +12,16 @@
         <!-- Icon pack description -->
         <div class="icon-pack-description-wrap">
             <div class="icon-pack-title">
-                Demo Icon Pack
+                22over7 Icon Pack
             </div>
             <div class="icon-pack-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Just released in time for Christmas, try 22over7's primere icon pack for free.
             </div>
             <div class="icon-pack-links">
-                <a class="icon-pack-link">
+                <a class="icon-pack-link" href="/?a=loadIcons&pack=demo" onclick="thankYou()">
                     Install now
                 </a><br />
-                <a class="icon-pack-link">
+                <a class="icon-pack-link" href="/?a=loadIcons&pack=demo&customize&page=installer">
                     Customize first
                 </a>
             </div>
@@ -30,5 +29,4 @@
         <!-- end Icon pack description -->
 
     </div>
-    <?php } ?>
 </div>
