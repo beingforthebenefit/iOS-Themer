3ab44fc17bed966fe6f8350dc3e017a8c4b3ebc0
Gerald Todd <gerald@gtodd.dev>
2020-12-13 18:54:01 -0800

    Update JS to restore GSAP transistions.

--- public/js/main.js
+++ public/js/main.js
@@ -31,28 +31,18 @@ function move(e){
 gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
 gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
 
+// main transitions
+gsap.from('.main__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.main__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
+gsap.from('.main__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+
 // homepage
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
 gsap.from('.try_now_button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
-
-
 
 // howtopage
-gsap.from('.how-to-title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.how-to-description', {opacity: 0, duration: 1, delay:1.8, y: 30})
 gsap.from('.store-button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.how-to-img', {opacity: 0, duration: 1, delay:1.3, y: 30})
-
 
 // icon-packs-page
-gsap.from('.icon-packs-title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.icon-packs-description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.icon-packs-img', {opacity: 0, duration: 1, delay:1.3, y: 30})
-
 
 // iosthemer
-gsap.from('.iosthemer-title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.iosthemer-description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.iosthemer-img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+gsap.from('.installer', {opacity: 0, duration: 1, delay: 1.3, y: 30})
