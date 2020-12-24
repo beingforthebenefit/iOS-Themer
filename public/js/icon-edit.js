6a00bc94ca8f12dbd49c06fe0b0b0b6d62c4c111
Gerald Todd <gerald@gtodd.dev>
2020-12-24 01:29:37 -0800

    Enable setting blank labels.

--- public/js/icon-edit.js
+++ public/js/icon-edit.js
@@ -1,6 +1,10 @@
 function editIcon(index, name) {
     var newName = prompt("Edit icon name", name);
-    if (newName && newName != name) {
+    if (newName == '') {
+        newName = ' ';
+    }
+    if (newName != null && newName != name) {
         window.location.replace("/?a=renameIcon&name=" + newName + "&index=" + index + "#phone");
     }
+    console.log(newName);
 }
