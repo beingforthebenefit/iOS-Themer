a47ff93b1a5e91898850380005ad7a4cc0f19ea4
Gerald Todd <gerald@gtodd.dev>
2020-12-24 03:39:08 -0800

    Restyle the icon pack page with previews.

--- application/views/home/icon-packs.php
+++ application/views/home/icon-packs.php
@@ -1,21 +1,23 @@
 <link rel="stylesheet" href="/css/card-style.css<?= $u ?>">
 <div class="container">
-    <div class="card">
+    <div class="card" style="background:url(/images/icon-pack-previews/demo.jpg)">
         <div class="content">
             <h3>Demo Icon Pack</h3>
+            <a href="/?a=loadIcons&pack=demo&customize&page=installer">Customize Icons/Labels</a><br />
+            <a href="/?a=loadIcons&pack=demo">Install Now</a>
             <p>Free Demo Icon Pack.  Download the Full Version in the Store!
             </p>
-          <a href="/?a=loadIcons&pack=demo">Install Now</a><br />
-          <a href="/?a=loadIcons&pack=demo&customize&page=installer">Edit first</a>
         </div>
     </div>
 
-    <div class="card">
+    <div class="card" style="background:url(/images/icon-pack-previews/minimal-red.jpg)">
         <div class="content">
             <h3>Minimal Red</h3>
             <p>Minimal Red Themed Icon Pack for iOS14.3+ 
             </p>
-            <a class="gumroad-button" href="https://gum.co/RtqjQ" >$12 - Buy Now</a>
+            <a href="/?a=loadIcons&pack=minimal-red-basic">Download Free Sample Instantly</a><br />
+            <a class="gumroad-button" href="https://gum.co/RtqjQ" >Buy Now Full Version</a><br />
+            <p>Full version includes ability to edit icons and labels and 120+ apps, 3rd party and new icons by request.</p>
         </div>
     </div>
 </div>
