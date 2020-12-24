84e7efb6abc4015ecba6dfa84d81c0528bf6c32f
Ubaid Dafedar <75362648+doyourhomeworkjacob@users.noreply.github.com>
2020-12-25 00:42:11 +0530

    Changed some text on in the Product Card UI
    Shortened the text on the Purchase Buttons, removed the Description under the Minimal Red text in the Minimal Red Icon Pack Card. Added Price for the Minimal Red Icon Pack

--- application/views/home/icon-packs.php
+++ application/views/home/icon-packs.php
@@ -3,9 +3,9 @@
     <div class="card" style="background:url(/images/icon-pack-previews/demo.jpg)">
         <div class="content">
             <h3>Demo Icon Pack</h3>
-            <a href="/?a=loadIcons&pack=demo&customize&page=installer">Customize Icons/Labels</a><br />
+            <a href="/?a=loadIcons&pack=demo&customize&page=installer">Edit Icon Pack</a><br />
             <a href="/?a=loadIcons&pack=demo">Install Now</a>
-            <p>Free Demo Icon Pack.  Download the Full Version in the Store!
+            <p>Free Demo Icon Pack<br>Full version will be released soon.
             </p>
         </div>
     </div>
@@ -13,11 +13,9 @@
     <div class="card" style="background:url(/images/icon-pack-previews/minimal-red.jpg)">
         <div class="content">
             <h3>Minimal Red</h3>
-            <p>Minimal Red Themed Icon Pack for iOS14.3+ 
-            </p>
-            <a href="/?a=loadIcons&pack=minimal-red-basic">Download Free Sample Instantly</a><br />
-            <a class="gumroad-button" href="https://gum.co/RtqjQ" >Buy Now Full Version</a><br />
-            <p>Full version includes ability to edit icons and labels and 120+ apps, 3rd party and new icons by request.</p>
+            <a href="/?a=loadIcons&pack=minimal-red-basic">Free Demo</a><br />
+            <a class="gumroad-button" href="https://gum.co/RtqjQ" >Buy Full Version - $12</a><br />
+            <p>Full version includes 120+ icons and ability to edit icons/labels.</p>
         </div>
     </div>
 </div>
