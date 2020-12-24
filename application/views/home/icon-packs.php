d3a09ffd3037995c1c721eac10ed5ce9b77a3801
Gerald Todd <gerald@gtodd.dev>
2020-12-23 23:57:27 -0800

    Make icon pack links active and change a style.

--- application/views/home/icon-packs.php
+++ application/views/home/icon-packs.php
@@ -1,18 +1,12 @@
+<link rel="stylesheet" href="/css/card-style.css<?= $u ?>">
 <div class="container">
     <div class="card">
         <div class="content">
             <h3>Demo Icon Pack</h3>
             <p>Free Demo Icon Pack.  Download the Full Version in the Store!
             </p>
-          <a href="#">Free!</a>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="content">
-            <h3>Pastel Pink</h3>
-            <p>Custom-Designed Pastel Pink themed Icon Pack for iOS14.3+.<br>Includes - 120+ Custom Icons, 5 Custom Wallpapers, Lifetime Updates </p>
-              <a href="#">$8</a>
+          <a href="/?a=loadIcons&pack=demo">Install Now</a><br />
+          <a href="/?a=loadIcons&pack=demo&customize&page=installer">Edit first</a>
         </div>
     </div>
 
@@ -21,7 +15,7 @@
             <h3>Minimal Red</h3>
             <p>Minimal Red Themed Icon Pack for iOS14.3+ 
             </p>
-            <a href="#">$16</a>
+            <a href="https://gumroad.com/l/RtqjQ">$16 - Buy Now</a>
         </div>
     </div>
 </div>
