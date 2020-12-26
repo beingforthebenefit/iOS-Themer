4658db8bded2e79acde750fd93f2fcbe5fd3e6ac
Gerald Todd <gerald@gtodd.dev>
2020-12-26 12:06:47 -0800

    Remove traf demo page.

--- public/traf/Instructions_files/32.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[32],{E6VO:function(n,i,d){"use strict";d.r(i),d.d(i,"Divider",(function(){return t}));var e=d("nKUr"),r=d("dTf1"),t=function(n){return Object(e.jsx)("div",{id:Object(r.a)(n.id),className:"notion-divider"})}}}]);
