07d913f4cae3a248a90ac6b28566113d017e9009
Gerald Todd <gerald@gtodd.dev>
2021-01-27 04:11:54 -0800

    Retrieve Info about our icon packs from Flurly in useful manor
    some notes:
    1. the icon pack name in Flurly must match exactly with the directory where the icons are which, in turn, must match the icon preivew pack.
    2. All icon preview images must be \`jpg\` for dumb reasons

--- application/models/PackModel.class.php
+++ application/models/PackModel.class.php
@@ -2,5 +2,21 @@
 // application/models/PackModel.class.php
 
 class PackModel extends Model {
+	function getAllIconPacks() {
 
+		$url = 'https://flurly.com/api/store_details/22over7';
+		$cURL = curl_init();
+		curl_setopt($cURL, CURLOPT_URL, $url);
+		curl_setopt($cURL, CURLOPT_HTTPGET, true);
+		curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($cURL, CURLOPT_HTTPHEADER, [
+		    'Content-Type: application/json',
+		    'Accept: application/json'
+		]);
+
+		$result = curl_exec($cURL);
+		curl_close($cURL);
+
+		return json_decode($result, true);
+	}
 }
